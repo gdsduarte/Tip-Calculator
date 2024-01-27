153c98ca2de72c319eefd3fa94ac5c1a6995f456
gdsduarte <gds_duarte@hotmail.com>
2024-01-27 22:25:38 +0000

    First Commit

--- app/src/main/java/com/example/tipcalculator/TipCalculatorActivity.kt
+++ app/src/main/java/com/example/tipcalculator/TipCalculatorActivity.kt
@@ -7,36 +7,43 @@ import java.text.NumberFormat
 
 class TipCalculatorActivity : AppCompatActivity() {
 
+    // Binding object instance with access to the views in the
+    // activity_tip_calculator.xml layout
     private lateinit var binding: ActivityTipCalculatorBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Inflate the layout XML file and return a binding object instance
         binding = ActivityTipCalculatorBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Setup a click listener on the calculate button to calculate the tip
         binding.calculateButton.setOnClickListener { calculateTip() }
     }
 
+    // Calculate the tip and display on the screen
     private fun calculateTip() {
+        // Get the decimal value from the cost of service EditText field
         val stringInTextField = binding.costOfService.text.toString()
+        // Convert the text to a decimal value
         val cost = stringInTextField.toDoubleOrNull()
+        // If the cost is null or 0, then display 0 tip and exit this function early
         if (cost == null) {
             binding.tipResult.text = ""
             return
         }
-
+        // Get the tip percentage based on which radio button is selected
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
-
+        // Calculate the tip
         var tip = tipPercentage * cost
         if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
-
+        // Display the formatted tip value on screen
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
